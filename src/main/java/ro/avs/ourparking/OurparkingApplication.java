41735d4aa9de27bb0a8d8b1a17257fe7cc67e78d
Viorel Alexandrescu <alex.viorel.117@gmail.com>
2019-10-31 14:10:53 +0200

    Initial Commit

---
+++ src/main/java/ro/avs/ourparking/OurparkingApplication.java
@@ -0,0 +1,13 @@
+package ro.avs.ourparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/avs/ourparking/OurparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.avs.ourparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurparkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
