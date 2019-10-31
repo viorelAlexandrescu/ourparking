49064fd034230b1e939437a8f682e788d2dd5762
Viorel Alexandrescu <alex.viorel.117@gmail.com>
2019-10-31 14:11:51 +0200

    fixing build problems

--- src/main/java/ro/avs/ourparking/OurParkingApplication.java
+++ src/main/java/ro/avs/ourparking/OurParkingApplication.java
@@ -1,13 +1,14 @@
 package ro.avs.ourparking;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
 
 @SpringBootApplication
 public class OurParkingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OurParkingApplication.class, args);
+		run(OurParkingApplication.class, args);
 	}
 
 }
