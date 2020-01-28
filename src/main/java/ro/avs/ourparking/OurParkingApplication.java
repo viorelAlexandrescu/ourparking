827045452c759e766af66c3df2274983dc64f37f
ViorelAlexandrescu <alex.viorel.117@gmail.com>
2020-01-28 14:45:19 +0200

    removed key and refactored for dynamic key config

--- src/main/java/ro/avs/ourparking/OurParkingApplication.java
+++ src/main/java/ro/avs/ourparking/OurParkingApplication.java
@@ -4,10 +4,9 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 import static org.springframework.boot.SpringApplication.run;
 
@@ -18,7 +17,8 @@ public class OurParkingApplication {
         run(OurParkingApplication.class, args);
 
         try {
-            InputStream serviceAccount = new ClassPathResource("/parkingmanagement-e0ab0-firebase-adminsdk-fdxgb-9580a225b1.json").getInputStream();
+            String apiKeyPath = System.getenv("firebase_api_key");
+            FileInputStream serviceAccount = new FileInputStream(apiKeyPath);
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .setDatabaseUrl("https://parkingmanagement-e0ab0.firebaseio.com")
